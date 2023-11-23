2a42393cd55c33d46b7a7c39125e80e746ace0f7
ahgodev <143968195+ahgodev@users.noreply.github.com>
2023-11-23 14:54:40 -0300

    Código js do login comentado

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 
+// Ao carregar a página
 window.onload = function logadologin() {
-    const login2 = document.querySelector(".login");
+    //Pega o botão de entrar 
+    const login = document.querySelector(".login");
+    //Pega o nome do usuário
     let username = document.querySelector("#username");
+    //Pega a div que mostra o nome e a opcção de sair
     let user_and_logout = document.querySelector("#user_and_logout");
     
+    //Se o usuário está logado
     if (localStorage.getItem("logado") == "true") {
-        login2.style.display = "none";
+        //O botão de entrar para de ser exibido, e a div, com o nome de usuário
+        //e o botão de sair, aparece no lugar de 'entrar'
+        login.style.display = "none";
         user_and_logout.style.display = "flex";
         username.innerHTML = localStorage.getItem("name");
     }
 
+    //Quando o usuário clica em sair, a página recarrega e a opção de entrar volta a aparecer
     let logout = document.getElementById("logout");
     logout.addEventListener('click', function(){
         localStorage.setItem("logado", "false");
